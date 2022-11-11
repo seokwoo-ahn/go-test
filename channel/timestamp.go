524224e16665d6098a222d34b468dcc21f85d7d6
seokwoo-ahn <answ2005@gmail.com>
2022-11-11 10:10:46 +0900

    Chore: merge timestamp case

--- channel/timestamp.go
+++ channel/timestamp.go
@@ -29,3 +29,26 @@ func WithTimeStamp() {
 	time.Sleep(time.Second * 10)
 	ticker.Stop()
 }
+
+func WithoutTimeStamp() {
+	ch := make(chan int)
+
+	go func() {
+		for {
+			for i := 0; ; i++ {
+				ch <- i
+				fmt.Println("channel input:", ch, time.Now())
+			}
+		}
+	}()
+
+	ticker := time.NewTicker(time.Second)
+	go func() {
+		for range ticker.C {
+			fmt.Println("tick:", <-ch)
+		}
+	}()
+
+	time.Sleep(time.Second * 10)
+	ticker.Stop()
+}
