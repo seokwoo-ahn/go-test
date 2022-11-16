048a46a97d82be2e891b94e6acccb7cd233da977
seokwoo-ahn <answ2005@gmail.com>
2022-11-17 00:18:54 +0900

    Feat: empty array length

---
+++ array/arraylen.go
@@ -0,0 +1,8 @@
+package array
+
+import "fmt"
+
+func EmptyArray() {
+	var test []string
+	fmt.Println(len(test))
+}
---
+++ array/arraylen_test.go
@@ -0,0 +1,9 @@
+package array
+
+import (
+	"testing"
+)
+
+func TestSelect(t *testing.T) {
+	EmptyArray()
+}
