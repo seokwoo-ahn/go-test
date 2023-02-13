bb9d8e8f9204de8d4f8bb96a52aa556457aff6cc
seokwoo-ahn <answ2005@gmail.com>
2023-02-13 10:50:41 +0900

    Feat: load configs

--- type_test/json.go
+++ type_test/json.go
@@ -1,12 +1,34 @@
 package types
 
-type Configs struct {
+import (
+	"encoding/json"
+	"flag"
+	"os"
+)
+
+type configs struct {
 	Port     int    `json:"port"`
 	Url      string `json:"url"`
-	Database Database
+	Database database
 }
 
-type Database struct {
+type database struct {
 	Name       string `json:"name"`
 	Collection string `json:"collection"`
+}
+
+func loadConfigs() (*configs, error) {
+	c := new(configs)
+	var configFlag = flag.String("config", "./config.json", "configuration json file path")
+	if file, err := os.Open(*configFlag); err != nil {
+		return &configs{}, err
+	} else {
+		defer file.Close()
+		if err := json.NewDecoder(file).Decode(c); err != nil {
+			return &configs{}, err
+		} else {
+			// c.sanitize()
+			return c, nil
+		}
+	}
 }
--- type_test/json_test.go
+++ type_test/json_test.go
@@ -6,25 +6,46 @@ import (
 )
 
 func TestJsonMarshal(t *testing.T) {
-	db := Database{
-		Name:       "db_name_test",
-		Collection: "collection_test",
-	}
-	cfg := Configs{
-		Port:     443,
-		Url:      "seok-study.tistory.com",
-		Database: db,
-	}
+	t.Run("marshal json struct", func(t *testing.T) {
+		db := database{
+			Name:       "db_name_test",
+			Collection: "collection_test",
+		}
+		cfg := configs{
+			Port:     443,
+			Url:      "seok-study.tistory.com",
+			Database: db,
+		}
 
-	b, err := json.Marshal(&cfg)
-	if err != nil {
-		panic(err)
-	}
-	t.Log(string(b))
+		b, err := json.Marshal(&cfg)
+		if err != nil {
+			panic(err)
+		}
+		t.Log(string(b))
 
-	b, err = json.MarshalIndent(&cfg, "*", "    ")
-	if err != nil {
-		panic(err)
-	}
-	t.Log(string(b))
+		b, err = json.MarshalIndent(&cfg, "*", "    ")
+		if err != nil {
+			panic(err)
+		}
+		t.Log(string(b))
+	})
+
+	t.Run("load configs", func(t *testing.T) {
+		cfg, err := loadConfigs()
+		if err != nil {
+			panic(err)
+		}
+
+		b, err := json.Marshal(&cfg)
+		if err != nil {
+			panic(err)
+		}
+		t.Log(string(b))
+
+		b, err = json.MarshalIndent(&cfg, "!", "  ")
+		if err != nil {
+			panic(err)
+		}
+		t.Log(string(b))
+	})
 }
